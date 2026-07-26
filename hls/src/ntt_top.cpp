e03c8335c92e44112056b3ed9d40f54e533729b0
Aditya Muthukattu <19423025+AdityaMuthukattu@users.noreply.github.com>
2026-07-27 01:23:57 +1000

    uncommented tripcount pragmas

--- hls/src/ntt_top.cpp
+++ hls/src/ntt_top.cpp
@@ -93,14 +93,15 @@ void ntt(int16_t r[256]) {
 
   k = 1;
   for(len = 128; len >= 2; len >>= 1) {
-    // #pragma HLS LOOP_TRIPCOUNT min=7 max=7
+    #pragma HLS LOOP_TRIPCOUNT min=7 max=7
     for(start = 0; start < 256; start += (len << 1)) {
-      // #pragma HLS LOOP_TRIPCOUNT min=1 max=64
+      // #pragma HLS LOOP_FLATTEN
+      #pragma HLS LOOP_TRIPCOUNT min=1 max=64
       zeta = zetas[k++];
       for(j = start; j < start + len; j++) {
         #pragma HLS PIPELINE II=1
         #pragma HLS DEPENDENCE variable=local_r type=inter dependent=false
-        // #pragma HLS LOOP_TRIPCOUNT min=2 max=128
+        #pragma HLS LOOP_TRIPCOUNT min=2 max=128
         t = fqmul(zeta, local_r[j + len]);
         local_r[j + len] = local_r[j] - t;
         local_r[j] = local_r[j] + t;
