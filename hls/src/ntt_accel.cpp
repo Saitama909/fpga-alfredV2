664eaf14c12193e82d03c991fbb33b3c94f6c8d0
Jovan Xin <jovan3.1415926@gmail.com>
2026-08-07 12:59:17 +1000

    Add 512-bit AXI bus (down to 3.619us/NTT)

--- hls/src/ntt_accel.cpp
+++ hls/src/ntt_accel.cpp
@@ -2,12 +2,22 @@
 #include "ntt_top.h"
 
 extern "C" void ntt_accel(int16_t r[256]) {
-#pragma HLS INTERFACE m_axi port=r offset=slave bundle=gmem \
+// 32 16-bit coefficients per AXI transfer word, so drops 256-element read burst cycles from 128 cycles down to 8 cycles!
+
+// // notes for myself - to allow FPGA to do DDR requests/recieve data nd what not.
+#pragma HLS INTERFACE m_axi port=r offset=slave bundle=gmem \ // Base address supplied by AXI-Lite slave reg
     depth=256 max_widen_bitwidth=512 max_read_burst_length=256 max_write_burst_length=256
+
+    // AXI-Lite Slave Control Interface Pragmas:
+    // Exposes control register 0x00 (ap_start/ap_done) and 0x10/0x14 (pointer r)
+    
+// AXI lite slave interface to write and read
 #pragma HLS INTERFACE s_axilite port=r bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-    int16_t local_r[256];
+// BRAm array 
+// Cyclic factor=32 partitions memory into 32 parallel BRAM 
+int16_t local_r[256];
 #pragma HLS ARRAY_PARTITION variable=local_r cyclic factor=32 dim=1
 
 read_burst:
@@ -15,7 +25,7 @@ read_burst:
 #pragma HLS PIPELINE II=1
         local_r[i] = r[i];
     }
-
+    
     ntt(local_r);
 
 write_burst:
